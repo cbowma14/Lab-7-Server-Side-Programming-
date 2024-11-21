fcd5d943d6791b96c6c920b34f187e11f6c394de
cbowma14 <129985140+cbowma14@users.noreply.github.com>
2024-11-21 14:43:09 -0500

    Added Lab5 Files

---
+++ Services/ILibraryService.cs
@@ -0,0 +1,21 @@
+﻿using Lab5.Models;
+
+namespace Lab5.Services
+{
+    public interface ILibraryService
+    {
+
+        public List<Book> ReadBooks();
+        public List<User> ReadUsers();
+        public void AddBook(Book book);
+        public void EditBook(int id, string newTitle, string newAuthor, string newISBN);
+        public void DeleteBook(int id);
+        public void AddUser(User user);
+        public void EditUser(int id, string newName, string newEmail);
+        public void DeleteUser(int id);
+
+
+
+
+    }
+}
